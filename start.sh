a6389b6800fb86382e7bf4fe65f96fe422161158
Ross Money <5647850+rossmoney@users.noreply.github.com>
2022-08-25 19:37:55 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 
 echo "Starting PHP-FPM..."
-/usr/sbin/php-fpm8.0 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/usr/sbin/php-fpm8.1 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Queue Work..."
 php /home/container/webroot/artisan queue:work --verbose --tries=3 --timeout=90 &
